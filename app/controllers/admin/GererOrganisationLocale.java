eb36d9fb916accc91569c7d70b59471cc6f9e7fd
ScalpGrizden <Nicolas.brandt@eleves.ec-nantes.fr>
2015-03-12 20:37:50 +0100

    Création des formulaires d'ajout et d'update pour les groupes locaux

--- app/controllers/admin/GererOrganisationLocale.java
+++ app/controllers/admin/GererOrganisationLocale.java
@@ -19,6 +19,8 @@ import java.io.IOException;
 import functions.UploadImage;
 import play.mvc.Security;
 import views.html.admin.organisationLocale.gererOrganisationLocale;
+import views.html.admin.organisationLocale.editerGroupe;
+import views.html.admin.organisationLocale.ajouterGroupe;
 
 @Security.Authenticated(SecuredMembre.class)
 public class GererOrganisationLocale extends Controller{
@@ -30,5 +32,49 @@ public class GererOrganisationLocale extends Controller{
             return Admin.nonAutorise();
         }
     }
-
+    
+    public static Result edit(Integer groupe_id) {
+    	Groupe groupe=Groupe.find.where().eq("groupe_id", groupe_id).findUnique();
+    	return ok(editerGroupe.render("",groupe));
+    }
+    
+    public static Result changerInfos(Integer groupe_id) {
+    	Groupe groupe=Groupe.find.where().eq("groupe_id", groupe_id).findUnique();
+		if(groupe!=null){
+			DynamicForm df = DynamicForm.form().bindFromRequest();
+			String groupe_nom = df.get("groupe_nom");
+			if(Groupe.find.where().eq("groupe_nom", groupe_nom).findUnique()==null){
+				groupe.groupe_nom=groupe_nom;
+			}
+			Integer pere_id = Integer.parseInt(df.get("pere"));
+			Groupe pere=Groupe.find.where().eq("groupe_id", pere_id).findUnique();
+			groupe.groupe_pere = pere;
+			String type_intitule = df.get("type");
+			TypeGroupementLocal type=TypeGroupementLocal.find.where().eq("type_groupement_local_intitule", type_intitule).findUnique();
+			groupe.groupe_type = type;
+			groupe.update();
+		}
+		return ok(editerGroupe.render("Informations mises à jour avec succès",groupe));
+	}
+    
+    public static Result add() {
+    	return ok(ajouterGroupe.render(""));
+    }
+    
+    public static Result ajouterInfos() {
+    	DynamicForm df = DynamicForm.form().bindFromRequest();
+    	String groupe_nom = df.get("groupe_nom");
+    	String type_intitule = df.get("type");
+    	String pere_string = df.get("pere");
+    	if (pere_string.equals("NULL")){
+    	Groupe groupe= new Groupe(groupe_nom,type_intitule);
+    	groupe.save();
+    	} else {
+    	Integer pere_id = Integer.parseInt(pere_string);
+    	Groupe pere=Groupe.find.where().eq("groupe_id", pere_id).findUnique();
+    	Groupe groupe= new Groupe(groupe_nom,type_intitule,pere);
+    	groupe.save();
+    	}
+    	return ok(ajouterGroupe.render("Informations mises à jour avec succès"));
+    }
 }
